1a20a4b084fd6c807b336836adac1e7fbb294246
Tring-Asbar <asbar.j@tringapps.com>
2025-01-28 13:05:50 +0530

    week1-git-AddNewLine

--- GitCommands.java
+++ GitCommands.java
@@ -1,5 +1,7 @@
 public class  GitCommands{
     public static void main(String[] args){
-        System.out.println("Hello Tringapps");
+        System.out.println("Hello Tringapps"); //Initial Commit
+        System.out.println("Thanks for giving me this wonderful opportunity"); //Add New Line
     }
+
 }
